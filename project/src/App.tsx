58351a54632aaed49b9d5e37b09a2ae0cf77c4ce
yigit025 <yigitcagdur@gmail.com>
2025-07-14 15:28:51 +0300

    App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,6 +5,7 @@ import { Hero } from './components/Hero';
 import { Projects } from './components/Projects';
 import { About } from './components/About';
 import { Services } from './components/Services';
+import { Hero } from './components/HeroVideo';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { Phone, MessageCircle } from 'lucide-react';
